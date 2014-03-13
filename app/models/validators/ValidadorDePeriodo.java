92c1de71f200d039c908d510f7763a094c74a113
Caynan <caynanvls@gmail.com>
2014-03-13 15:33:28 -0300

    Adicionado estrategias de validacao para adicionar disciplina nos  periodos

--- app/models/validators/ValidadorDePeriodo.java
+++ app/models/validators/ValidadorDePeriodo.java
@@ -1,10 +1,5 @@
 package models.validators;
 
-import models.Cadeira;
-import models.Periodo;
-import models.exceptions.LimiteDeCreditosException;
-
-
 public interface ValidadorDePeriodo {
-	public boolean valida(Periodo periodo) throws LimiteDeCreditosException;
+	abstract public boolean valida(int novaQuantidadeCreditos);
 }
--- app/models/validators/ValidadorMax.java
+++ app/models/validators/ValidadorMax.java
@@ -1,18 +1,20 @@
 package models.validators;
 
+import javax.naming.LimitExceededException;
+
 import models.Periodo;
 import models.exceptions.LimiteDeCreditosException;
 
-public class ValidadorMax implements ValidadorDePeriodo{
+public class ValidadorMax implements ValidadorDePeriodo {
 
 	public static final int MAXIMO_DE_CREDITOS = 28;
-	
-	@Override
-	public boolean valida(Periodo periodo) throws LimiteDeCreditosException {
-		if(periodo.getCreditos() > MAXIMO_DE_CREDITOS){
-			throw new LimiteDeCreditosException("Limite de Créditos Ultrapassado");
+
+	public boolean valida(int novaQuantidadeCreditos) {
+		boolean isNovaQtdCreditosValida = true;
+		if (novaQuantidadeCreditos > MAXIMO_DE_CREDITOS) {
+			isNovaQtdCreditosValida = false;
 		}
-		return true;
+		return isNovaQtdCreditosValida;
 	}
 
 }
