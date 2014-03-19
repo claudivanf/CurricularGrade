a72271700c4715c6b5a7c9d839f7e55776bdbe18
Caynan <caynanvls@gmail.com>
2014-03-19 11:43:06 -0300

    validadores de credito, para verificar se periodocontinua adequado ao adicionar disciplina

--- app/models/validators/ValidadorMax.java
+++ app/models/validators/ValidadorMax.java
@@ -2,14 +2,14 @@ package models.validators;
 
 import models.exceptions.LimiteDeCreditosException;
 
-public class ValidadorMax implements ValidadorDePeriodo{
+public class ValidadorMax implements ValidadorDePeriodo {
 
 	public static final int MAXIMO_DE_CREDITOS = 28;
-	
-	@Override
+
 	public boolean valida(int creditos) throws LimiteDeCreditosException {
-		if(creditos > MAXIMO_DE_CREDITOS){
-			throw new LimiteDeCreditosException("Limite de Créditos Ultrapassado");
+		if (creditos > MAXIMO_DE_CREDITOS) {
+			throw new LimiteDeCreditosException(
+					"Limite de Créditos Ultrapassado");
 		}
 		return true;
 	}
